9b6fbf47f2e2e57bbfaf118079046a3dc45be7be
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-03-15 12:23:45 +0100

    added constants for ROUTES; changed references in corresponding files
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { ROUTES } from '../common/constants.js';
+
 	export let segment;
 </script>
 
@@ -50,11 +52,14 @@
 
 <nav>
 	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+		<li>
+			<a aria-current='{segment === undefined ? "page" : undefined}' href='{ROUTES.HOME}'>home</a>
+		</li>
+		<li>
+			<a aria-current='{segment === ROUTES.ABOUT ? "page" : undefined}' href='{ROUTES.ABOUT}'>about</a>
+		</li>
+		<li>
+			<a rel='prefetch' aria-current='{segment === ROUTES.EXPERIMENTS ? "page" : undefined}' href='{ROUTES.EXPERIMENTS}'>experiments</a>
+		</li>
 	</ul>
 </nav>
--- src/routes/experiments/[slug].svelte
+++ src/routes/experiments/[slug].svelte
@@ -1,8 +1,10 @@
 <script context="module">
+	import { ROUTES } from '../../common/constants.js';
+
 	export async function preload({ params, query }) {
 		// the `slug` parameter is available because
 		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
+		const res = await this.fetch(`${ROUTES.EXPERIMENTS}/${params.slug}.json`);
 		const data = await res.json();
 
 		if (res.status === 200) {
